07e98b1039408f17d3a214e38463ba0bf3577a2b
Levi Leal <leviplj@gmail.com>
2014-08-10 23:48:18 -0300

    Teste de valicação de campos

--- cliente/forms.py
+++ cliente/forms.py
@@ -1,7 +1,19 @@
 from django import forms
 from cliente.models import Cliente
-    
+from django.forms.util import ErrorList
+
 class ClienteForm(forms.ModelForm):
-    class Meta:
-        model = Cliente
-        exclude = ['endereco']
+	class Meta:
+		model = Cliente
+		exclude = ['endereco']
+		read_only = ['data_cadastro']
+
+	def clean(self):
+		cleaned_data = self.cleaned_data
+		nome = cleaned_data.get('nome');
+
+		if 'levi' not in nome:
+			self._errors['nome'] = ErrorList(['esqueceu de levi']) 
+
+		# Sempre retorne a coleção completa de dados válidos.
+		return cleaned_data
