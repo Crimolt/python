0e911e8e2cdb779c938ccd085d9a1fe76b6ba205
Levi Leal <leviplj@gmail.com>
2014-08-20 22:53:07 -0300

    Atualizando database

--- nathan/settings.py
+++ nathan/settings.py
@@ -12,11 +12,11 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'nathan',                      # Or path to database file if using sqlite3.
+        'NAME': 'python',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'USER': 'admincdkaylj',
+        'PASSWORD': 'Zf1wcGgWmY-r',
+        'HOST': 'postgresql://$OPENSHIFT_POSTGRESQL_DB_HOST:$OPENSHIFT_POSTGRESQL_DB_PORT',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
 }
