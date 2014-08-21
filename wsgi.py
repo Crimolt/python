8c5179d6abe0574a5141631d15f8bd4b89cde525
Levi Leal <leviplj@gmail.com>
2014-08-21 00:26:20 -0300

    teste

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,8 @@
-status = '200 OK'
-response_headers = [('Content-Type', ctype), ('Content-Length', str(len(response_body)))]
-#
-start_response(status, response_headers)
-return[response_body]
+def application(environ, start_response):
+	ctype = 'text/plain'
+	response_body = "1"
+	status = '200 OK'
+	response_headers = [('Content-Type', ctype), ('Content-Length', str(len(response_body)))]
+	#
+	start_response(status, response_headers)
+	return[response_body]
